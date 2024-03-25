830bdc6f2f9bdaef2b040ba0002c1f0179f2c0ba
HoangDH <daohuyhoang9119@gmail.com>
2024-03-25 21:06:42 +0700

    Update project

--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"Golang-crud/config"
+	"Golang-crud/controller"
 	"Golang-crud/helper"
+	"Golang-crud/repository"
+	"Golang-crud/router"
+	"Golang-crud/service"
 	"fmt"
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func main()  {
 	fmt.Printf("startt")
-	routes := httprouter.New()
-	routes.GET("/",func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		fmt.Fprint(w,"HEEEEEEEE")
-	})
+	db := config.DatabaseConnection()
+
+	// repository
+	bookRepository := repository.NewBookRepository(db)
+
+	// service
+	bookService := service.NewBookServiceImpl(bookRepository)
+
+	// controller
+	bookController := controller.NewBookController(bookService)
+
+	// router
+	routes := router.NewRouter(bookController)
+	
 
 	server := http.Server{Addr: "localhost:8888", Handler: routes}
 	err := server.ListenAndServe()
---
+++ service/book_service.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"Golang-crud/data/request"
+	"Golang-crud/data/response"
+	"context"
+)
+
+type BookService interface {
+	Create(ctx context.Context, request request.BookCreateRequest)
+	Update(ctx context.Context, request request.BookUpdateRequest)
+	Delete(ctx context.Context, bookId int)
+	FindById(ctx context.Context, bookId int) response.BookResponse
+	FindAll(ctx context.Context) []response.BookResponse
+}
